6066d258486d5d89f68f9ea44caeaf76afe48877
akki2332 <akashsoam123456789@gmail.com>
2026-02-01 14:42:24 +0530

    PORT

--- backend/server.js
+++ backend/server.js
@@ -72,7 +72,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Signaling server running on http://localhost:${PORT}`);
 });
